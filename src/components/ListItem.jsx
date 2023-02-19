2a5982fb3b60a60cfdac77d518e301d8846126a9
mwafrika <mwafrikajosue@gmail.com>
2023-02-19 21:27:17 +0200

    refactor codes

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -5,38 +5,39 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import {
   faTrash,
-  faTrashAlt,
-  faEdit,
   faPencil,
-  faEllipsisV,
   faSortDown,
 } from "@fortawesome/free-solid-svg-icons";
 
 const ListItem = ({ buttonText, onButtonClick, ...rest }) => {
+  const {
+    id,
+    jobTitle,
+    country,
+    city,
+    Type,
+    status,
+    Opening_date,
+    Closing_date,
+    Organization,
+    Languages,
+    number_of_applicants,
+  } = rest;
+
   return (
     <div className={styles.jobCardContainer}>
       <div className={styles.jobHeader}>
         <div className={styles.applicants}>
-          <div>{rest.number_of_applicants}</div>
+          <div>{number_of_applicants}</div>
           <div>Applicants</div>
         </div>
         <div className={styles.headerContainer}>
           <div className={styles.jobTitle}>
             <div>
-              <h1>{rest.jobTitle}</h1>
+              <h1>{jobTitle}</h1>
             </div>
             <div className={styles.actions}>
-              <div
-                style={{
-                  width: "42.5px",
-                  height: "42.5px",
-                  backgroundColor: "black",
-                  borderRadius: "10px",
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
+              <div className={styles.actions_container}>
                 <FontAwesomeIcon
                   icon={faPencil}
                   style={{
@@ -45,17 +46,7 @@ const ListItem = ({ buttonText, onButtonClick, ...rest }) => {
                   }}
                 />
               </div>
-              <div
-                style={{
-                  width: "42.5px",
-                  height: "42.5px",
-                  backgroundColor: "rgb(190, 33, 38)",
-                  borderRadius: "10px",
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
+              <div className={styles.button_container}>
                 <FontAwesomeIcon
                   icon={faTrash}
                   style={{
@@ -78,30 +69,28 @@ const ListItem = ({ buttonText, onButtonClick, ...rest }) => {
             </div>
           </div>
           <div className={styles.headerDetails}>
-            <div className={styles.country}>{rest.country}</div>
-            <div className={styles.city}>{rest.city}</div>
-            <div className={styles.type}>{rest.Type}</div>
-            <div className={styles.status}>{rest.status}</div>
+            <div className={styles.country}>{country}</div>
+            <div className={styles.city}>{city}</div>
+            <div className={styles.type}>{Type}</div>
+            <div className={styles.status}>{status}</div>
           </div>
         </div>
       </div>
       <div className={styles.headerBody}>
         <h3>
-          From <span className={styles.custom_style}>{rest.Opening_date}</span>
+          From <span className={styles.custom_style}>{Opening_date}</span>
         </h3>
         <h3>
-          To <span className={styles.custom_style}>{rest.Closing_date}</span>
+          To <span className={styles.custom_style}>{Closing_date}</span>
         </h3>
         {" | "}
         <h3>Organization</h3>
         <h3>
-          <span className={styles.custom_style}>{rest.Organization}</span>
+          <span className={styles.custom_style}>{Organization}</span>
         </h3>{" "}
         |<h3>Languages</h3>
         <h3>
-          <span className={styles.custom_style}>
-            {rest.Languages.join(",  ")}
-          </span>
+          <span className={styles.custom_style}>{Languages.join(",  ")}</span>
         </h3>
       </div>
 
